eaf67e6fe4b4183e39dce93af19343c83c1f4486
Edy Segura <edysegura@gmail.com>
2017-08-07 23:10:19 -0400

    Example implemented

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 A simple example to create a pure JavaScript component. In order to have the pokemons images run the command below:
 
 ```node catch-them-all.js```
+
+## References
+
+- [Creating pure ES6 components - Portuguese](https://willianjusten.com.br/criando-componentes-usando-so-es6/)
---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+function createMarkup(pokes) {
+    return pokes.map( poke => `
+        <figure>
+            <img src="images/${poke.id}.png" alt="${poke.name} picture">
+            <figcaption>${poke.name}</figcaption>
+        </figure>
+    `).join('')
+}
+
+function byType(type) {
+    return function filterByType(pokemon) {
+        return pokemon.type.toLowerCase().includes(type)
+    }
+}
+
+const pokesContainer = document.querySelector('.pokemons')
+const pokes = pokeDB.filter(byType('water'))
+
+pokesContainer.innerHTML = createMarkup(pokes)
